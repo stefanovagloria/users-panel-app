1455415f7e1d4dce2e289960a7415247113c4363
stefanovagloria <gloriqstefanova97@gmail.com>
2023-10-23 19:26:09 +0300

    Header and Footer components created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import {Fragment} from 'react';
 import './App.css';
 import { Header } from './components/Header';
+import {Footer} from './components/Footer';
 
 function App() {
   return (
     <Fragment>
       <Header/>
+
+      <Footer/>
     </Fragment>
   );
 }
